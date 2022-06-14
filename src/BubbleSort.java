1610a551734a9d7775ecc49ccb2093229da36384
VinitGrover <vinitgrover255@gmail.com>
2022-06-14 10:46:26 +0530

    Add uc4

---
+++ src/BubbleSort.java
@@ -0,0 +1,17 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        String[] arr = {"e", "a", "c", "z", "h"};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].compareTo(arr[j]) > 0) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (String x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
