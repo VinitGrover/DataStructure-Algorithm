04c2ed22bf6999963a0361be163b87141d972231
VinitGrover <vinitgrover255@gmail.com>
2022-06-14 10:34:53 +0530

    Add uc3

--- src/com/bridgelabz/algorithms/FindWord.java
+++ src/com/bridgelabz/algorithms/FindWord.java
@@ -1,23 +1,39 @@
 package com.bridgelabz.algorithms;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class FindWord {
-    public static void main(String[] args) {
-        String findWord[] = {"Ramesh", "Rajesh", "Mohit", "Rohit"};
 
-        System.out.println("Enter the word you want to find ");
-        String search = new Scanner(System.in).nextLine();
-        boolean flag = false;
-
-        for (String found : findWord) {
-            if (found.equals(search)) {
-                System.out.println(found + " has been found");
-                flag = true;
-                break;
+    static int find(String search, String[] findWord, int l, int length) {
+        if (length >= l) {
+            int mid = l + (length - l) / 2;
+            if (findWord[mid] == search) {
+                return mid;
+            } else if (findWord[mid].compareTo(search) > 0) {
+                return find(search, findWord, l, mid - 1);
+            } else {
+                return find(search, findWord, mid + 1, length);
             }
         }
-        if(flag==false)
-            System.out.println(search +" not found");
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String findWord[] = {"Ramesh", "Rajesh", "Mohit", "Rohit"};
+        String search = "Rohit";
+
+        Arrays.sort(findWord);
+
+        for (String x : findWord) {
+            System.out.print(x + " ");
+        }
+
+        int i = find(search, findWord, 0, findWord.length - 1);
+        if (i == -1) {
+            System.out.println("Word is not in the Array");
+        } else {
+            System.out.println("\nWord found at sorted Array of index : " + (i + 1));
+        }
+
     }
 }
